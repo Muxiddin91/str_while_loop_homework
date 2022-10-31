5f24f03693b1bf0abab441ceb892b250ae2621c4
muxiddin <muxiddintemirov@gmail.com>
2022-10-31 10:04:58 +0500

    10 bajarildi

--- While10.py
+++ While10.py
@@ -6,4 +6,14 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    
+    uzunligi=len(s)
+    a=0
+    c=0
+    while a<uzunligi:
+        b=s[a]
+        if int(b)%2==1:
+            c+=int(b)
+        a+=1
+    return c
+print(main('22992'))
